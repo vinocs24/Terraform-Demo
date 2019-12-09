f993057a76daa1a76867af83268303e239632ab4
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 00:00:04 +0530

    Update subnet.tf

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -27,3 +27,28 @@ resource "aws_subnet" "demo-vpc-subnet3" {
         Name = "demo-vpc-subnet"
     }
 }
+
+output "subnet1-id" {
+  value = aws_subnet.demo-vpc-subnet1.id
+}
+
+output "subnet2-id" {
+  value = aws_subnet.demo-vpc-subnet2.id
+}
+
+output "subnet3-id" {
+  value = aws_subnet.demo-vpc-subnet3.id
+}
+
+
+output "subnet1-cidr" {
+  value = aws_subnet.demo-vpc-subnet1.cidr_block
+}
+
+output "subnet2-cidr" {
+  value = aws_subnet.demo-vpc-subnet2.cidr_block
+}
+
+output "subnet3-cidr" {
+  value = aws_subnet.demo-vpc-subnet3.cidr_block
+}
