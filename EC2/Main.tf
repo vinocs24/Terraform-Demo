1bc9d5d9ec15df801c391a6c791aee7491e09451
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-09 14:50:02 +0530

    Update Main.tf

--- EC2/Main.tf
+++ EC2/Main.tf
@@ -1,1 +1,13 @@
-
+resource "aws_launch_configuration" "ecs-launch-configuration" {
+    name                        = var.launch-configuration-name
+    image_id                    = var.image-id
+    instance_type               = var.instance-type
+    security_groups             = [var.security-group-id, var.rds-security-group]
+    associate_public_ip_address = true
+    key_name                    = var.ecs-key-pair-name
+    user_data                   = file("installing-components.sh")
+    
+    tags = {
+      Name = "ec2-instance"
+    }
+}
