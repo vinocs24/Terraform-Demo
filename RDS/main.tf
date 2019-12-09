1442fe33db434f68187eddf5c50c5d299b18dd4e
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 01:41:20 +0530

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "rds" {
-  identifier             = "var.environment"
+  identifier             = var.environment
   allocated_storage      = var.allocated_storage
   engine                 = "mysql"
   engine_version         = "5.7"
