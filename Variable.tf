7e857dda8003b91b73f64009f1ad85d3c932ba71
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 01:09:27 +0530

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -2,3 +2,19 @@ variable "key_name" {
   description = "SSH key name to access the EC2 instances"
   default     = "rds-key"
 }
+
+/*====
+environment specific variables
+======*/
+
+variable "production_database_name" {
+  description = "The database name for Production"
+}
+
+variable "production_database_username" {
+  description = "The username for the Production database"
+}
+
+variable "production_database_password" {
+  description = "The user password for the Production database"
+}
