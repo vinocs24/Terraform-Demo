57ce898a90403cf31236b8c3d8cac5f7da04c2b6
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 00:57:53 +0530

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -14,7 +14,7 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 /* Security Group for resources that want to access the Database */
 resource "aws_security_group" "db_access_sg" {
-  vpc_id      = "var.vpc_id
+  vpc_id      = var.vpc_id
   name        = var.environment-db-access-sg
   description = "Allow access to RDS"
 
