8ded65e771dfb573faac3a6e35de4f5fd36d7e32
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 00:15:46 +0530

    Update subnet.tf

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "demo-vpc-subnet1" {
     vpc_id     = aws_vpc.demo-vpc.id
     cidr_block = "10.0.0.0/24"
-    availability_zone = "us-west-2"
+    availability_zone = "us-west-2a"
 
     tags = {
         Name = "demo-vpc-subnet"
@@ -11,7 +11,7 @@ resource "aws_subnet" "demo-vpc-subnet1" {
 resource "aws_subnet" "demo-vpc-subnet2" {
     vpc_id     = aws_vpc.demo-vpc.id
     cidr_block = "10.0.1.0/24"
-    availability_zone = "us-west-2a"
+    availability_zone = "us-west-2b"
 
     tags = {
         Name = "demo-vpc-subnet"
@@ -21,7 +21,7 @@ resource "aws_subnet" "demo-vpc-subnet2" {
 resource "aws_subnet" "demo-vpc-subnet3" {
     vpc_id     = aws_vpc.demo-vpc.id
     cidr_block = "10.0.2.0/24"
-    availability_zone = "us-west-2b"
+    availability_zone = "us-west-2c"
 
     tags = {
         Name = "demo-vpc-subnet"
