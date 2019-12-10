570fb6217fac34a8b0f2f5efb5b634032e612c0c
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 16:33:59 +0530

    Update output.tf

--- RDS/output.tf
+++ RDS/output.tf
@@ -2,9 +2,9 @@ output "rds_address" {
   value = aws_db_instance.rds.address
 }
 
-output "db_access_sg_id" {
-  value = aws_security_group.db_access_sg.id
-}
+#output "db_access_sg_id" {
+#  value = aws_security_group.db_access_sg.id
+#}
 
 output "rds_user" {
   value = aws_db_instance.rds.username
