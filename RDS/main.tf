75168d9c1b7cc77ee9b21f84c4808bc4df84c245
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 14:28:16 +0530

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -42,6 +42,37 @@ resource "aws_security_group" "db_access_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+  
+resource "aws_security_group" "rds_sg" {
+  name = "var.environment-rds-sg"
+  description = "var.environment-Security Group"
+  vpc_id = var.vpc_id
+  
+  // allows traffic from the SG itself
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      self = true
+  }
+
+  //allow traffic for TCP 5432
+  ingress {
+      from_port = 3306
+      to_port   = 3306
+      protocol  = "tcp"
+      security_groups = [aws_security_group.rds_sg.id]
+  }
+
+  // outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
 
 output "rds-security-group" {
   value = aws_security_group.db_access_sg.id
