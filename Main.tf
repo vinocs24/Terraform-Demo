a1b7812fc2ec8c5c826829271ce7489c854b1a11
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 13:57:45 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -18,11 +18,10 @@ module "VPC" {
 
 module "EC2" {
     source = "./EC2"
-    source = "./RDS"
 
     vpc-id                      = "module.vpc.id"
     security-group-id           = "module.vpc.security-group-id"
-    rds-security-group          = "module.rds.db_access_sg_id"
+    rds-security-group          = "module.vpc.db_access_sg_id"
     subnet-id-1                 = "module.vpc.subnet1-id"
     subnet-id-2                 = "module.vpc.subnet2-id"
     key_name                    = var.key_name
