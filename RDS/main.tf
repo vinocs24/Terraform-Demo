b04372e73be2f59adb3d4d2b64c20ade9804ff43
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 14:47:05 +0530

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -17,30 +17,6 @@ resource "aws_security_group" "db_access_sg" {
   vpc_id      = var.vpc_id
   name        = "var.environment-db-access-sg"
   description = "Allow access to RDS"
-
-  // allows traffic from the SG itself
-  ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      self = true
-  }
-
-  //allow traffic for TCP 5432
-  ingress {
-      from_port = 3306
-      to_port   = 3306
-      protocol  = "tcp"
-      security_groups = [aws_security_group.db_access_sg.id]
-  }
-
-  // outbound internet access
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
   
 resource "aws_security_group" "rds_sg" {
